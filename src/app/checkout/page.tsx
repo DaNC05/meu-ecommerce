991a5c25dd97632c215876586095164447226cd8
Daniel Nogueira <danienog.carvalho@gmail.com>
2026-04-15 20:12:09 -0300

    Cria página de checkout com proteção de rota e simulação de pagamento

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,38 +1,19 @@
 'use client'
 
 import { useContext, useEffect, useState } from "react";
-import { CartContext } from "@/contexts/CartContext";
-import styled from "styled-components";
+import { CartContext } from '@/contexts/CartContext';
+import { AuthContext } from '@/contexts/AuthContext';
 
 
-const Container = styled.div`
-     max-width: 800px;
-     margin: 0 auto;
-     padding:20px;
-    `
-const ItemEstilizado = styled.div`
-     display:flex;
-     justify-content:space-between;
-     align-items: center;
-     border-bottom:1px solid #eee
-    `
-
-const BotaoAcao = styled.button`
-     padding:5px 10px;
-     border:none;
-     border-radius:5px;
-     cursor:pointer;
-     background-color:${props => props.$remover ? 'red' : '#f0f0f0'};
-     &:hover{background-color:${props => props.$remover ? 'darkred' : '#e0e0e0'}};
-    `
-
-
-export default function PaginaCarrinho() {
-    const { carrinho, removeDoCarrinho, aumentarQuantidade, diminuirQuantidade, limparCarrinho } = useContext(CartContext)
-    const [carregando, setCarregando] = useState(true)
-
-    // Estado para guardar os dados completos vindos da API
+export default function Checkout() {
+    const { carrinho, limparCarrinho, aumentarQuantidade, diminuirQuantidade, removeDoCarrinho } = useContext(CartContext);
+    const { usuario, Login, Logout } = useContext(AuthContext)
     const [produtosDoCarrinho, setProdutosDoCarrinho] = useState([])
+    const [carregando, setCarregando] = useState(true)
+    const confirmarPagamento = () => {
+        alert("Compra Realizada com Sucesso")
+        limparCarrinho()
+    }
 
     useEffect(() => {
         async function buscarProdutos() {
@@ -42,6 +23,9 @@ export default function PaginaCarrinho() {
             setCarregando(false)
         } buscarProdutos()
     }, [carrinho])
+    if (usuario === null) {
+        return <><h2>Faça login para finalizar a compra</h2> <button onClick={Login}>Entrar</button></>
+    }
     if (carregando) {
         return <h2>Carregando produtos... ⏳</h2>
     }
@@ -52,8 +36,8 @@ export default function PaginaCarrinho() {
 
     }, 0)
     return (
-        <Container>
-            <h1>Seu Carrinho</h1>
+        <div>
+            <h1>Pagamento</h1>
             {carrinho.length === 0 ? (<h2>Seu Carrinho está vazio</h2>) :
                 (<>
                     {carrinho.map((item) => {
@@ -61,22 +45,24 @@ export default function PaginaCarrinho() {
 
                         const subTotal = (produtoCompleto?.price || 0) * item.quantidade
                         return (
-                            <ItemEstilizado key={item.produtoId}>
+                            <div key={item.produtoId}>
                                 <p >
                                     Produto: {produtoCompleto?.title} | Quantidade: {item.quantidade} | Valor: {subTotal.toFixed(2)}
                                 </p>
-                                <BotaoAcao onClick={() => aumentarQuantidade(item.produtoId)}>+</BotaoAcao>
-                                <BotaoAcao onClick={() => diminuirQuantidade(item.produtoId)}>-</BotaoAcao>
-                                <BotaoAcao $remover onClick={() => removeDoCarrinho(item.produtoId)}>❌</BotaoAcao>
-                            </ItemEstilizado>
+                                <button onClick={() => aumentarQuantidade(item.produtoId)}>+</button>
+                                <button onClick={() => diminuirQuantidade(item.produtoId)}>-</button>
+                                <button $remover onClick={() => removeDoCarrinho(item.produtoId)}>❌</button>
+                            </div>
                         )
                     })}
                     {<h2>Total da Compra: R$ {valorTotal.toFixed(2)}</h2>}
-                    <BotaoAcao onClick={limparCarrinho}>Limpar Carrinho</BotaoAcao>
+                    <button onClick={confirmarPagamento}>Confirmar Pagamento</button>
+                    <button onClick={limparCarrinho}>Limpar Carrinho</button>
                 </>)
             }
 
-        </Container>
+        </div>
     )
-}
 
+
+}
